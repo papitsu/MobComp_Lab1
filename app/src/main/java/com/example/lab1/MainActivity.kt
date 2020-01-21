e1120c758082b1d4d958aeda2263e8acd13c719d
papitsu <panu.molsa@hotmail.com>
2020-01-21 12:30:08 +0200

    add buttons WIP

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get reference to button
+        val testButton = findViewById(R.id.button) as Button
+
+        testButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Its toast!", Toast.LENGTH_SHORT).show()
+        }
+
+        // get reference to button
+        val mapButton = findViewById(R.id.mapButton) as Button
+        mapButton.setOnClickListener {
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/lab1/MapActivity.kt
@@ -0,0 +1,14 @@
+package com.example.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+
+    }
+}
